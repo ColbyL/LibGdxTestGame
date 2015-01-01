019547f085d1f2e2435eb5cf5a364f2995f05bec
Colby L <colby.leclerc@gmail.com>
2015-01-01 11:44:33 -0500

    First Commit

---
+++ desktop/src/TestGame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package TestGame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import TestGame.MyGdxTestGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxTestGame(), config);
+	}
+}
